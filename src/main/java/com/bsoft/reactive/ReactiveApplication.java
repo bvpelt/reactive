6ccbce4001f13305d2c022be4655d2bb99ea9a85
bvpelt <bart.vanpelt@gmail.com>
2025-08-26 16:23:53 +0200

    WIP

--- src/main/java/com/bsoft/reactive/ReactiveApplication.java
+++ src/main/java/com/bsoft/reactive/ReactiveApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReactiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReactiveApplication.class, args);
+        SpringApplication.run(ReactiveApplication.class, args);
 	}
 
 }
